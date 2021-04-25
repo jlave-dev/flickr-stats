5a0fe15c97de42a594b6f22008379770f0326282
James Lave <30740613+jlave-dev@users.noreply.github.com>
2021-04-24 19:32:48 -0500

    ✅ Add fake data for tests

---
+++ spec/data/photo-samples.ts
@@ -0,0 +1,42 @@
+export default [
+    {
+        id: 'v8MifE103ejFEL3GC2Kh',
+        photo_id: '51124616241',
+        sampled: '2021-04-24T16:41:33.084Z',
+        views: 148,
+        faves: 6,
+        comments: 0,
+    },
+    {
+        id: 'QGR-WtTMVgs6wSrUK_1r',
+        photo_id: '51124616241',
+        sampled: '2021-04-23T03:21:37.067Z',
+        views: 137,
+        faves: 5,
+        comments: 0,
+    },
+    {
+        id: '7QRoDMh62iexHPKDzNMk',
+        photo_id: '51120283466',
+        sampled: '2021-04-24T16:41:33.091Z',
+        views: 198,
+        faves: 7,
+        comments: 1,
+    },
+    {
+        id: 'T-_iLopYAOhmCo4IjFxG',
+        photo_id: '51120283466',
+        sampled: '2021-04-23T03:21:37.068Z',
+        views: 190,
+        faves: 6,
+        comments: 1,
+    },
+    {
+        id: '-yha9iPFFEJ1DrezxRXl',
+        photo_id: '51118604353',
+        sampled: '2021-04-24T16:41:33.093Z',
+        views: 243,
+        faves: 13,
+        comments: 6,
+    },
+];
