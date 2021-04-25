dbe497c18f514d708e02d53e4d892c8da0f7fcdb
James Lave <30740613+jlave-dev@users.noreply.github.com>
2021-04-24 19:53:35 -0500

    ✅ Add jasmine-spec-reporter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
         node: true,
         jasmine: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 12,
@@ -17,5 +13,6 @@ module.exports = {
     plugins: ['@typescript-eslint', 'prettier'],
     rules: {
         'prettier/prettier': 'error',
+        '@typescript-eslint/no-var-requires': 'off',
     },
 };
