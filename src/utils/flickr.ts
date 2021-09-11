bfdb7eed353249d1dd89c75501b196bda74f6df1
James Lave <30740613+jlave-dev@users.noreply.github.com>
2021-09-10 22:15:45 -0500

    ♻️ Get photo and follower counts without scraping

--- src/utils/flickr.ts
+++ src/utils/flickr.ts
@@ -1,69 +1,7 @@
-import axios from 'axios';
 import Flickr from 'flickr-sdk';
 import { FlickrAPIPhoto, User, UserSample } from '../types';
 
 export const client = new Flickr(process.env.FLICKR_CONSUMER_KEY);
-
-export async function getUserData(): Promise<User> {
-    const flickrRes = await client.profile.getProfile({
-        user_id: process.env.FLICKR_USER_ID,
-        per_page: 500,
-        extras: 'with_stats',
-    });
-
-    const { profile } = flickrRes.body;
-
-    return {
-        id: profile.id,
-        joined: new Date(profile.join_date * 1000).toISOString(),
-        occupation: profile.occupation,
-        hometown: profile.hometown,
-        first_name: profile.first_name,
-        last_name: profile.last_name,
-        facebook: profile.facebook,
-        twitter: profile.twitter,
-        tumblr: profile.tumblr,
-        instagram: profile.instagram,
-        pinterest: profile.pinterest,
-    };
-}
-
-export async function getUserStats(): Promise<Omit<UserSample, 'id' | 'sampled'>> {
-    // First scrape profile page to get follow and photo counts
-    const axiosRes = await axios.get(`https://www.flickr.com/people/${process.env.FLICKR_USER_ID}/`);
-
-    if (axiosRes.status !== 200) {
-        throw new Error(`Request to user page failed: ${axiosRes.statusText}`);
-    }
-
-    const html: string = axiosRes.data;
-
-    // TODO: Replace the follows regexes to account for 999 -> 1K
-    const followersMatch = html.match(/(\d+) Followers/);
-    const followingMatch = html.match(/(\d+) Following/);
-    const photosMatch = html.match(/(\d+|\d{1,3}(,\d{3})*)(\.\d+)? Photos/);
-
-    // Then make an API request to get other stats
-    const flickrRes = await client.profile.getProfile({
-        user_id: process.env.FLICKR_USER_ID,
-        per_page: 500,
-        extras: 'with_stats',
-    });
-
-    const { stats } = flickrRes.body;
-
-    return {
-        user_id: process.env.FLICKR_USER_ID as string,
-        followers: parseInt((followersMatch && followersMatch[1]) as string, 10),
-        following: parseInt((followingMatch && followingMatch[1]) as string, 10),
-        photos: parseInt(((photosMatch && photosMatch[1]) as string).replace(/,/g, ''), 10),
-        views: stats.photo_views,
-        tags: stats.tags,
-        geotags: stats.geotagged,
-        faves: stats.faves,
-        groups: stats.groups,
-    };
-}
 
 export async function getPhotos(): Promise<FlickrAPIPhoto[]> {
     const extras = [
@@ -91,4 +29,59 @@ export async function getPhotos(): Promise<FlickrAPIPhoto[]> {
     });
 
     return res.body.photos.photo;
+}
+
+export async function getUserData(): Promise<User> {
+    const flickrRes = await client.profile.getProfile({
+        user_id: process.env.FLICKR_USER_ID,
+        per_page: 500,
+        extras: 'with_stats',
+    });
+
+    const { profile } = flickrRes.body;
+
+    return {
+        id: profile.id,
+        joined: new Date(profile.join_date * 1000).toISOString(),
+        occupation: profile.occupation,
+        hometown: profile.hometown,
+        first_name: profile.first_name,
+        last_name: profile.last_name,
+        facebook: profile.facebook,
+        twitter: profile.twitter,
+        tumblr: profile.tumblr,
+        instagram: profile.instagram,
+        pinterest: profile.pinterest,
+    };
+}
+
+export async function getUserStats(): Promise<Omit<UserSample, 'id' | 'sampled'>> {
+    // First make an API request to get follow and photo counts
+    let flickrRes = await client.people.getInfo({
+        user_id: process.env.FLICKR_USER_ID,
+        extras: ['contacts', 'rev_contacts'].join(','),
+    });
+
+    const { person } = flickrRes.body;
+
+    // Then make an API request to get other stats
+    flickrRes = await client.profile.getProfile({
+        user_id: process.env.FLICKR_USER_ID,
+        per_page: 500,
+        extras: 'with_stats',
+    });
+
+    const { stats } = flickrRes.body;
+
+    return {
+        user_id: process.env.FLICKR_USER_ID as string,
+        followers: parseInt(person.rev_contacts._content, 10),
+        following: parseInt(person.contacts, 10),
+        photos: parseInt(person.photos.count._content, 10),
+        views: stats.photo_views,
+        tags: stats.tags,
+        geotags: stats.geotagged,
+        faves: stats.faves,
+        groups: stats.groups,
+    };
 }
